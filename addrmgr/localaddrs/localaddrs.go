31ad8b75fcffe40b8855b9d7d59b7c554391bd75
Caleb James DeLisle <cjd@cjdns.fr>
2022-08-02 20:24:32 +0200

    localaddrs.go use generic locking system

--- addrmgr/localaddrs/localaddrs.go
+++ addrmgr/localaddrs/localaddrs.go
@@ -3,22 +3,22 @@ package localaddrs
 import (
 	"net"
 	"strings"
-	"sync"
 
 	"github.com/pkt-cash/pktd/addrmgr"
+	"github.com/pkt-cash/pktd/btcutil/er"
+	"github.com/pkt-cash/pktd/btcutil/lock"
 	"github.com/pkt-cash/pktd/pktlog/log"
 	"github.com/pkt-cash/pktd/wire"
 )
 
 type LocalAddrs struct {
-	m        sync.Mutex
-	a        map[string]*wire.NetAddress
-	wasTried bool
+	a        lock.GenMutex[map[string]*wire.NetAddress]
+	wasTried lock.AtomicBool
 }
 
 func New() LocalAddrs {
 	return LocalAddrs{
-		a: make(map[string]*wire.NetAddress),
+		a: lock.NewGenMutex(make(map[string]*wire.NetAddress), "LocalAddrs lock"),
 	}
 }
 
@@ -26,9 +26,7 @@ func (la *LocalAddrs) Referesh() {
 	ifaces, errr := net.Interfaces()
 	if errr != nil {
 		log.Warnf("LocalAddrs.Referesh() failed: [%v]", errr.Error())
-		la.m.Lock()
-		defer la.m.Unlock()
-		la.wasTried = true
+		la.wasTried.Store(true)
 		return
 	}
 	out := make(map[string]struct{})
@@ -42,52 +40,60 @@ func (la *LocalAddrs) Referesh() {
 			out[a.String()] = struct{}{}
 		}
 	}
-	la.m.Lock()
-	la.wasTried = true
-	for s := range la.a {
-		if _, ok := out[s]; !ok {
-			log.Infof("Local address gone [%s]", log.IpAddr(s))
-			delete(la.a, s)
+	la.a.In(func(m *map[string]*wire.NetAddress) er.R {
+		for s := range *m {
+			if _, ok := out[s]; !ok {
+				log.Infof("Local address gone [%s]", log.IpAddr(s))
+				delete(*m, s)
+			}
 		}
-	}
-	for s := range out {
-		if _, ok := la.a[s]; !ok {
-			// drop the port
-			spl := strings.Split(s, "/")
-			ip := net.ParseIP(spl[0])
-			if ip == nil {
-				log.Warnf("LocalAddrs.Referesh(): unable to parse addr [%s]", s)
-			} else {
-				wip := wire.NewNetAddressIPPort(ip, 0, 0)
-				if (addrmgr.IsIPv4(wip) && !addrmgr.IsLocal(wip)) || addrmgr.IsRoutable(wip) {
-					log.Infof("Local address detected [%s]", log.IpAddr(s))
-					la.a[s] = wip
+		for s := range out {
+			if _, ok := (*m)[s]; !ok {
+				// drop the port
+				spl := strings.Split(s, "/")
+				ip := net.ParseIP(spl[0])
+				if ip == nil {
+					log.Warnf("LocalAddrs.Referesh(): unable to parse addr [%s]", s)
 				} else {
-					log.Debugf("Non-routable local address detected [%s]", s)
-					la.a[s] = nil
+					wip := wire.NewNetAddressIPPort(ip, 0, 0)
+					if (addrmgr.IsIPv4(wip) && !addrmgr.IsLocal(wip)) || addrmgr.IsRoutable(wip) {
+						log.Infof("Local address detected [%s]", log.IpAddr(s))
+						(*m)[s] = wip
+					} else {
+						log.Debugf("Non-routable local address detected [%s]", s)
+						(*m)[s] = nil
+					}
 				}
 			}
 		}
-	}
-	la.m.Unlock()
+		return nil
+	})
 }
 
 func (la *LocalAddrs) IsWorking() bool {
-	la.m.Lock()
-	defer la.m.Unlock()
-	return len(la.a) > 0 || !la.wasTried
+	if !la.wasTried.Load() {
+		// We don't yet know...
+		return true
+	}
+	ok := false
+	la.a.In(func(m *map[string]*wire.NetAddress) er.R {
+		ok = len(*m) > 0
+		return nil
+	})
+	return ok
 }
 
 func (la *LocalAddrs) Reachable(na *wire.NetAddress) bool {
 	out := false
-	la.m.Lock()
-	for _, localNa := range la.a {
-		if localNa != nil && addrmgr.Reachable(localNa, na) {
-			log.Infof("[%s] reachable via [%s]", na.IP.String(), localNa.IP.String())
-			out = true
-			break
+	la.a.In(func(m *map[string]*wire.NetAddress) er.R {
+		for _, localNa := range *m {
+			if localNa != nil && addrmgr.Reachable(localNa, na) {
+				log.Infof("[%s] reachable via [%s]", na.IP.String(), localNa.IP.String())
+				out = true
+				break
+			}
 		}
-	}
-	la.m.Unlock()
+		return nil
+	})
 	return out
 }
